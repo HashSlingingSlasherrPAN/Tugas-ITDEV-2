0ab87dbe59ddea985b4cd5fe88efaed088671392
Adrella Arte <oujireflect.me@gmail.com>
2024-09-08 16:12:09 +0700

    Replace source code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Halaman sudah di load")
+    getProduct()
+})
 
-});
+function getProduct(){
+    fetch('https://fakestoreapi.com/products')
+        .then(respone => {
+            console.log(respone.json())
 
-fetch('https://fakestoreapi.com/products')
-        .then(response=>response.json())
-        .then(json=>{
-            console.log(json);
         })
+        .then(data => {
+            console.log('data: ', data)
+        })
+}
