3d7cff250ae1f2d281c5851450e3020ede8a3162
ChemicalSpark <rangga.pramudya23@gmail.com>
2024-09-09 22:05:59 +0700

    kurang lebih udah muncul dummyjsonnya

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Halaman sudah di load")
-    getProduct()
+    console.log("Halaman sudah di load");
+    getProduct();
 })
 
 function getProduct(){
-    fetch('https://fakestoreapi.com/products')
-        .then(respone => {
-            console.log(respone.json())
-        })
+    fetch('https://dummyjson.com/products')
+        .then(res => res.json())
         .then(data => {
-            data.forEach(element => {
-                const productList = document.querySelector('.product-container');
-                productList.insertAdjacentHTML('beforeend',
-                `<div class="product-container">
-                    <div class="card border-radius">
+            var container = document.querySelector('#product-container');
+            data.products.forEach(product => {
+                // product.description = shortText(product.description, 70);
+                // product.title = shortText(product.title, 20);
+                container.insertAdjacentHTML('beforeend',
+                    `<div class="card border-radius">
                         <div class="card-img">
-                            <img src="$entity(title)" alt="blackarch">
+                            <img src="${product.image}" alt="blackarch">
                         </div>
                         <section>
                             <header>
-                                <p class="fw-bold">Black Archlinux</p>
+                                <p class="fw-bold">${product.title}</p>
                             </header>
-                                <p>Lopem Ipsum Dolor</p>
+                                <p>${product.description}</p>
                                 <button class="bg-blue border-radius shadow pad-2">Detail Product</button>
                         </section>
                     </div>`
